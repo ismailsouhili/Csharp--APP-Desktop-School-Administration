db817fca1796e2f1ae706be20f40ae7d8dabcd08
Ismail Souhili <83651385+ismailsouhili@users.noreply.github.com>
2021-10-18 07:56:44 +0000

    Create Frm_Menu.Designer.cs

---
+++ Prj_DeutschSprachInstitut/Frm_Menu.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace Prj_DeutschSprachInstitut
+{
+    partial class Frm_Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Menu));
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.btnAusgang = new System.Windows.Forms.Button();
+            this.btnTeilnehmer = new System.Windows.Forms.Button();
+            this.btnAdmin = new System.Windows.Forms.Button();
+            this.btnTrainer = new System.Windows.Forms.Button();
+            this.btnKursen = new System.Windows.Forms.Button();
+            this.btnStudent = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelRight = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblsignup = new System.Windows.Forms.Label();
+            this.homepanel = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panelLeft.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelRight.SuspendLayout();
+            this.homepanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelLeft
+            // 
+            this.panelLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panelLeft.Controls.Add(this.btnAusgang);
+            this.panelLeft.Controls.Add(this.btnTeilnehmer);
+            this.panelLeft.Controls.Add(this.btnAdmin);
+            this.panelLeft.Controls.Add(this.btnTrainer);
+            this.panelLeft.Controls.Add(this.btnKursen);
+            this.panelLeft.Controls.Add(this.btnStudent);
+            this.panelLeft.Location = new System.Drawing.Point(3, 126);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(212, 508);
+            this.panelLeft.TabIndex = 4;
+            // 
+            // btnAusgang
+            // 
+            this.btnAusgang.BackColor = System.Drawing.Color.Gray;
+            this.btnAusgang.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAusgang.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.btnAusgang.ForeColor = System.Drawing.Color.White;
+            this.btnAusgang.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnAusgang.Location = new System.Drawing.Point(2, 302);
+            this.btnAusgang.Name = "btnAusgang";
+            this.btnAusgang.Size = new System.Drawing.Size(205, 52);
+            this.btnAusgang.TabIndex = 14;
+            this.btnAusgang.Text = "Ausgang";
+            this.btnAusgang.UseVisualStyleBackColor = false;
+            this.btnAusgang.Click += new System.EventHandler(this.btnAusgang_Click);
+            // 
+            // btnTeilnehmer
+            // 
+            this.btnTeilnehmer.BackColor = System.Drawing.Color.Gray;
+            this.btnTeilnehmer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTeilnehmer.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.btnTeilnehmer.ForeColor = System.Drawing.Color.White;
+            this.btnTeilnehmer.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnTeilnehmer.Location = new System.Drawing.Point(0, 238);
+            this.btnTeilnehmer.Name = "btnTeilnehmer";
+            this.btnTeilnehmer.Size = new System.Drawing.Size(205, 54);
+            this.btnTeilnehmer.TabIndex = 15;
+            this.btnTeilnehmer.Text = "Teilnehmer/ Kursen/ Noten";
+            this.btnTeilnehmer.UseVisualStyleBackColor = false;
+            this.btnTeilnehmer.Click += new System.EventHandler(this.btnTeilnehmer_Click_1);
+            // 
+            // btnAdmin
+            // 
+            this.btnAdmin.BackColor = System.Drawing.Color.Gray;
+            this.btnAdmin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdmin.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.btnAdmin.ForeColor = System.Drawing.Color.White;
+            this.btnAdmin.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnAdmin.Location = new System.Drawing.Point(0, 3);
+            this.btnAdmin.Name = "btnAdmin";
+            this.btnAdmin.Size = new System.Drawing.Size(206, 53);
+            this.btnAdmin.TabIndex = 10;
+            this.btnAdmin.Text = "Administration";
+            this.btnAdmin.UseVisualStyleBackColor = false;
+            this.btnAdmin.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btnTrainer
+            // 
+            this.btnTrainer.BackColor = System.Drawing.Color.Gray;
+            this.btnTrainer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTrainer.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.btnTrainer.ForeColor = System.Drawing.Color.White;
+            this.btnTrainer.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnTrainer.Location = new System.Drawing.Point(0, 62);
+            this.btnTrainer.Name = "btnTrainer";
+            this.btnTrainer.Size = new System.Drawing.Size(206, 55);
+            this.btnTrainer.TabIndex = 11;
+            this.btnTrainer.Text = "Die Trainer";
+            this.btnTrainer.UseVisualStyleBackColor = false;
+            this.btnTrainer.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // btnKursen
+            // 
+            this.btnKursen.BackColor = System.Drawing.Color.Gray;
+            this.btnKursen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKursen.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.btnKursen.ForeColor = System.Drawing.Color.White;
+            this.btnKursen.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnKursen.Location = new System.Drawing.Point(0, 180);
+            this.btnKursen.Name = "btnKursen";
+            this.btnKursen.Size = new System.Drawing.Size(205, 52);
+            this.btnKursen.TabIndex = 12;
+            this.btnKursen.Text = "Kursen";
+            this.btnKursen.UseVisualStyleBackColor = false;
+            this.btnKursen.Click += new System.EventHandler(this.btnKursen_Click);
+            // 
+            // btnStudent
+            // 
+            this.btnStudent.BackColor = System.Drawing.Color.Gray;
+            this.btnStudent.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStudent.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.btnStudent.ForeColor = System.Drawing.Color.White;
+            this.btnStudent.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnStudent.Location = new System.Drawing.Point(0, 123);
+            this.btnStudent.Name = "btnStudent";
+            this.btnStudent.Size = new System.Drawing.Size(205, 51);
+            this.btnStudent.TabIndex = 13;
+            this.btnStudent.Text = "Student";
+            this.btnStudent.UseVisualStyleBackColor = false;
+            this.btnStudent.Click += new System.EventHandler(this.btnStudent_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(207, 118);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panelRight
+            // 
+            this.panelRight.Controls.Add(this.label5);
+            this.panelRight.Controls.Add(this.lblsignup);
+            this.panelRight.Location = new System.Drawing.Point(216, 2);
+            this.panelRight.Name = "panelRight";
+            this.panelRight.Size = new System.Drawing.Size(1038, 118);
+            this.panelRight.TabIndex = 5;
+            this.panelRight.Paint += new System.Windows.Forms.PaintEventHandler(this.panelRight_Paint);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 18F);
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label5.Location = new System.Drawing.Point(3, 8);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(206, 30);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Willkommen bei";
+            // 
+            // lblsignup
+            // 
+            this.lblsignup.AutoSize = true;
+            this.lblsignup.Font = new System.Drawing.Font("Century Gothic", 18F);
+            this.lblsignup.ForeColor = System.Drawing.Color.Black;
+            this.lblsignup.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lblsignup.Location = new System.Drawing.Point(126, 50);
+            this.lblsignup.Name = "lblsignup";
+            this.lblsignup.Size = new System.Drawing.Size(311, 30);
+            this.lblsignup.TabIndex = 0;
+            this.lblsignup.Text = "DEUTSCH SPRACHINSTITUT";
+            this.lblsignup.Click += new System.EventHandler(this.lblsignup_Click);
+            // 
+            // homepanel
+            // 
+            this.homepanel.Controls.Add(this.pictureBox4);
+            this.homepanel.Controls.Add(this.pictureBox3);
+            this.homepanel.Controls.Add(this.pictureBox2);
+            this.homepanel.Location = new System.Drawing.Point(213, 126);
+            this.homepanel.Name = "homepanel";
+            this.homepanel.Size = new System.Drawing.Size(1041, 458);
+            this.homepanel.TabIndex = 6;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(753, 62);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(232, 208);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 12;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(394, 62);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(232, 208);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(33, 62);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(232, 208);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Frm_Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1242, 492);
+            this.Controls.Add(this.homepanel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panelRight);
+            this.Controls.Add(this.panelLeft);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Frm_Menu";
+            this.Text = "Frm_Menu";
+            this.panelLeft.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelRight.ResumeLayout(false);
+            this.panelRight.PerformLayout();
+            this.homepanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelLeft;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panelRight;
+        private System.Windows.Forms.Label lblsignup;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel homepanel;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button btnAusgang;
+        private System.Windows.Forms.Button btnTeilnehmer;
+        private System.Windows.Forms.Button btnAdmin;
+        private System.Windows.Forms.Button btnTrainer;
+        private System.Windows.Forms.Button btnKursen;
+        private System.Windows.Forms.Button btnStudent;
+    }
+}
