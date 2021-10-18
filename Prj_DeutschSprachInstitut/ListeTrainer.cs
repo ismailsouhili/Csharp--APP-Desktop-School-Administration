077f22cc5defe13884e5819e37236ee032d6a098
Ismail Souhili <83651385+ismailsouhili@users.noreply.github.com>
2021-10-18 07:55:06 +0000

    Create ListeTrainer.cs

---
+++ Prj_DeutschSprachInstitut/ListeTrainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prj_DeutschSprachInstitut
+{
+    public partial class ListeTrainer : Form
+    {
+        public ListeTrainer()
+        {
+            InitializeComponent();
+        }
+    }
+}
