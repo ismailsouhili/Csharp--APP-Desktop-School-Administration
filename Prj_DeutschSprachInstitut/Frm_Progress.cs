62fc06c6d83cb37e16923ebf9636769b98b03805
Ismail Souhili <83651385+ismailsouhili@users.noreply.github.com>
2021-10-18 07:56:30 +0000

    Create Frm_Progress.cs

---
+++ Prj_DeutschSprachInstitut/Frm_Progress.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prj_DeutschSprachInstitut
+{
+    public partial class Frm_Progress : Form
+    {
+        public Frm_Progress()
+        {
+            InitializeComponent();
+        }
+
+        int starpoint = 0;
+
+        private void Frm_Progress_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            starpoint += 1;
+            progres.Value = starpoint;
+            if (progres.Value == 100)
+            {
+                progres.Value = 0;
+                timer1.Stop();
+                this.Hide();
+                Frm_Login lg = new Frm_Login();
+                lg.Show();
+            }
+        }
+    }
+}
